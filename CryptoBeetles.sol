43110a196365984d0c10ca5e565cbb3949a6cf1a
manikantanynala97 <manikantanynala97@gmail.com>
2022-02-27 19:51:04 +0530

    NFT-COMMIT

---
+++ CryptoBeetles.sol
@@ -0,0 +1,29 @@
+
+
+// contracts/CryptoBettles.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract CryptoBeetles is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor(string memory _name , string memory _symbol) ERC721(_name,_symbol) {}
+
+    function mint( string memory tokenURI) // here we are minting our own nft and to ourself only
+        public
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+}
+
